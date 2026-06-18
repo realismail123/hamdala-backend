b6cc4dfd7de67c60d13e5c323fa6f50b0e93f67e
realismail123 <ismailusman47624@gmail.com>
2026-06-18 10:28:09 +0100

    Update server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
+import express from "express";
 
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "HAMDALA DATA Backend is running"
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
