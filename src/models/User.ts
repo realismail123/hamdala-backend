cc4ac99a145cf3cfd86b92b7fe4c2ae8d894dd09
realismail123 <ismailusman47624@gmail.com>
2026-06-25 14:08:52 +0100

    Create User model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,1 +1,19 @@
+import mongoose from "mongoose";
 
+const UserSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("User", UserSchema);
